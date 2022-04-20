14c4204c780aa40ac79a018a8039e199bbf9e3f5
YaraBlack <yara26072000@gmail.com>
2022-04-20 19:31:15 +0300

    Add Function and Simple object example

--- JS_Basics/vars_and_strictMode/JS/script.js
+++ JS_Basics/vars_and_strictMode/JS/script.js
@@ -14,30 +14,50 @@ const leftBorderWidth = 1; //can't be changed if the type isn't array or object.
 //Primitive data types
 //Boolean
 let bolleanVar = true;
+
 //Null
 let nullVar = null;
+
 //Undefined
 let undefinedVar;
 
 //Numeric
 //Number
 let numberVar = 3;
+
 //BigInt - ES11
 let bigIntVar = BigInt(Number.MAX_SAFE_INTEGER);
 console.log(bigIntVar);
+
 //NaN (Not a number)
 console.log(2 * "q");
 
 //String
 let stringVar = "qwerty";
+
 //Symbol - unique and immutable
 let symVar = Symbol("qwe");
 console.log(symVar);
 console.log(symVar.description);
 
 //Objects - collections of properties
-//Dates, Arrays, Functions, Objects
+//Date
 let today = new Date();
-console.log(today);
 
+//Array
 let qwew = [1, "w", true];
+
+//Functions
+function message() {
+  console.log("Greetings Linda!");
+}
+
+console.log(typeof message); // => function
+console.log(message instanceof Object); // => true
+
+//Simple object
+const obj = {
+  name: "wewe",
+  age: 111,
+  someBool: true,
+};
