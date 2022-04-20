e46d8ad7bb224cf49d8cd99a7627cb98d21a0be6
YaraBlack <yara26072000@gmail.com>
2022-04-20 22:00:18 +0300

    Show the work of alert, confirm and prompt methods

---
+++ JS_Basics/simple_interactions/JS/script.js
@@ -0,0 +1,16 @@
+// //Create a modal window with a message
+// alert("smth");
+
+// //Create a modal window with confirmation
+// const result = confirm("Confirm or deny");
+// console.log(result);
+
+// const answer = prompt("Write something", "placeholder"); //string
+// console.log(answer);
+
+const answers = [];
+answers[0] = prompt("www", "");
+answers[1] = prompt("qweqwe", "");
+answers[2] = prompt("asd", "");
+
+document.write(answers);
