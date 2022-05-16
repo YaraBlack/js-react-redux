21fb1d51c46f5d951189566cfbe4e2a71a4f1e39
YaraBlack <yara26072000@gmail.com>
2022-05-16 16:55:26 +0300

    create cards using fetch and json-server

--- JS_in_work/Food_site/js/modules/cardsClass.js
+++ JS_in_work/Food_site/js/modules/cardsClass.js
@@ -1,3 +1,5 @@
+import { getResource } from "./dataFlow.js";
+
 window.addEventListener("DOMContentLoaded", () => {
   class MenuCard {
     constructor(
@@ -46,34 +48,19 @@ window.addEventListener("DOMContentLoaded", () => {
     }
   }
 
-  new MenuCard(
-    "img/tabs/vegy.jpg",
-    "vegy",
-    'Меню "Фитнес"',
-    'Меню "Фитнес" - это новый подход к приготовлению блюд: больше свежих овощей и фруктов. Продукт активных и здоровых людей. Это абсолютно новый продукт с оптимальной ценой и высоким качеством!',
-    9,
-    ".menu .container"
-  ).render();
-
-  new MenuCard(
-    "img/tabs/elite.jpg",
-    "elite",
-    "Меню “Премиум",
-    "В меню “Премиум” мы используем не только красивый дизайн упаковки, но и качественное исполнение блюд. Красная рыба, морепродукты, фрукты - ресторанное меню без похода в ресторан!",
-    14,
-    ".menu .container",
-    "menu__item"
-  ).render();
-
-  new MenuCard(
-    "img/tabs/post.jpg",
-    "post",
-    'Меню "Постное"',
-    "Меню “Постное” - это тщательный подбор ингредиентов: полноеотсутствие продуктов животного происхождения, молоко из миндаля,овса, кокоса или гречки, правильное количество белков за счет тофуи импортных вегетарианских стейков.",
-    21,
-    ".menu .container",
-    "menu__item"
-  ).render();
+  getResource("http://localhost:3000/menu").then((data) => {
+    //              destructuring using ({})
+    data.forEach(({ img, altimg, title, descr, price }) => {
+      new MenuCard(
+        img,
+        altimg,
+        title,
+        descr,
+        price,
+        ".menu .container"
+      ).render();
+    });
+  });
 
   // let fitnessMenu = new MenuCard(
   //   "img/tabs/vegy.jpg",
